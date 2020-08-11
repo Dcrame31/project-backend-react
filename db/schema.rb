4831bf1dd81c97334cb5e72625eae52c324e83de
Diane Crame <dcrame@icloud.com>
2020-08-10 19:17:29 -0700

    added seed data and added column to campground

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_10_184431) do
+ActiveRecord::Schema.define(version: 2020_08_11_010029) do
 
   create_table "campgrounds", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_10_184431) do
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "cost"
   end
 
   create_table "locations", force: :cascade do |t|
