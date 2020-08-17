ad88471d8cd684c2843816c1b0bd8dbb2014128e
Diane Crame <dcrame@icloud.com>
2020-08-17 15:08:40 -0700

    add controller, serializer, and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_010029) do
+ActiveRecord::Schema.define(version: 2020_08_17_220249) do
 
   create_table "campgrounds", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_08_11_010029) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.string "comment"
+    t.integer "review_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
