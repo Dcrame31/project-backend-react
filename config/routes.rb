1772857ada3d33e80acc4a592890565c99f337b5
Diane Crame <dcrame@icloud.com>
2020-08-20 15:37:07 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, only: [:index, :create]
   resources :campgrounds
-  resources :locations
+  resources :locations, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
